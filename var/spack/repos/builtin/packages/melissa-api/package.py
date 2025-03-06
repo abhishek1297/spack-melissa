cbd3fee2b8bc5a98f49a2a91afdc220c61fdf2c1
Abhishek Purandare <abhishek.purandare@inria.fr>
2025-03-06 16:07:58 +0100

    fixed typo

--- var/spack/repos/builtin/packages/melissa-api/package.py
+++ var/spack/repos/builtin/packages/melissa-api/package.py
@@ -18,7 +18,7 @@ class MelissaApi(CMakePackage):
 
     license("BSD-3-Clause")
 
-    version("develop", branch="develop", deperecated=True)
+    version("develop", branch="develop", deprecated=True)
 
     depends_on("c", type="build")  # generated
     depends_on("fortran", type="build")  # generated
